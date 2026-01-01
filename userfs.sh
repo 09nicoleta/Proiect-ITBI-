ec8a9c3e63250bb9ec345048be1952b4bc86218c
09nicoleta <nicoleta.musat1@s.unibuc.ro>
2026-01-01 23:40:30 +0200

    Acum fisierul procs se actualizeaza. De asemenea, am adaugat un for care verifica daca un utilizator se deconecteaza, ulterior scriind in fisierul lastlogin data ultimei conectari.

--- userfs.sh
+++ userfs.sh
@@ -3,15 +3,37 @@
 cd /home/nic/proiect_itbi/director_radacina 
 a_users=($(users))
 
-for u in "${a_users[@]}"; do
+for user in "${a_users[@]}"; do
 
         #verificare daca exista directorul userului 
-    if [[ ! -d "$u" ]]; then
-        mkdir ./"$u"
-        touch ./"$u"/procs
+    if [[ ! -d "$user" ]]; then
+        mkdir ./"$user"
+        touch ./"$user"/procs
     fi
 
+    # #voi sterge sau ascunde fisierul lastlogin
+    # if [[ -f "./$user/lastlogin" ]]; then
+        
+    # fi
     
-   
+    ps -u "$user" -o pid,args > ./"$user"/procs
+
+    
 done
 
+for user in *; do
+    inact=1
+
+    for x in "${a_users[@]}"; do
+        if [[ "$x" == "$user" ]]; then
+            inact=0
+            break
+        fi
+    done
+
+    if [[ "$inact" == 1 ]]; then
+        >./"$user"/procs
+        lastlog -u "$user" | tail -n 1 | awk '{print $4, $5, $6, $9, $7}'>./"$user"/lastlogin
+        
+    fi
+done
