479bfdf0ddbc084bbe8d97e0e5cfb180748bf153
09nicoleta <nicoleta.musat1@s.unibuc.ro>
2026-01-03 12:49:56 +0200

    Acum fisierele se actualizeaza in fiecare 30 de secunde iar fisierul lastlogin acum apare doar cand utilizatorul este deconectat

--- userfs.sh
+++ userfs.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-cd /home/nic/proiect_itbi/director_radacina 
-a_users=($(users))
+cd "$HOME"/proiect_itbi/director_radacina 
 
-for user in "${a_users[@]}"; do
+while true; do
+    a_users=($(users))
 
-        #verificare daca exista directorul userului 
-    if [[ ! -d "$user" ]]; then
-        mkdir ./"$user"
-        touch ./"$user"/procs
-    fi
+    for user in "${a_users[@]}"; do
 
-    # #voi sterge sau ascunde fisierul lastlogin
-    # if [[ -f "./$user/lastlogin" ]]; then
-        
-    # fi
-    
-    ps -u "$user" -o pid,args > ./"$user"/procs
-
-    
-done
-
-for user in *; do
-    inact=1
-
-    for x in "${a_users[@]}"; do
-        if [[ "$x" == "$user" ]]; then
-            inact=0
-            break
+            #verificare daca exista directorul userului 
+        if [[ ! -d "$user" ]]; then
+            mkdir ./"$user"
+            touch ./"$user"/procs
         fi
+        ps -u "$user" -o pid,comm>"$user"/procs
+        #stergerea fisierului 
+        if [[ -f "./$user/lastlogin" ]]; then
+            rm "./$user/lastlogin" 
+
+        fi
+        
     done
 
-    if [[ "$inact" == 1 ]]; then
-        >./"$user"/procs
-        lastlog -u "$user" | tail -n 1 | awk '{print $4, $5, $6, $9, $7}'>./"$user"/lastlogin
-        
-    fi
+    for user in *; do
+        inact=1
+
+        for x in "${a_users[@]}"; do
+            if [[ "$x" == "$user" ]]; then
+                inact=0
+                break
+            fi
+        done
+
+        if [[ "$inact" == 1 ]]; then
+            >./"$user"/procs
+            lastlog -u "$user" | tail -n 1 | awk '{print $4, $5, $6, $9, $7}'>./"$user"/lastlogin
+            
+        fi
+    done
+    sleep 30
 done
