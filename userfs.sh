144929a1867e2c8f6a9630ed65234729e3aca139
09nicoleta <nicoleta.musat1@s.unibuc.ro>
2026-01-05 17:29:07 +0200

    Am efectuat 2 schimbari, si anume 1) am realizat ca eficienta programului scade daca un user se conecteaza de mai multe ori, de aceea am modificat felul in care se actualizeaza variabila a_users (acum are users unici) si 2) inainte daca fisierul userfs.sh era executat pe un alt shell fata de al meu, acesta nu rula (aparea eroare din vina liniei cd /home/nic/proiect_itbi/director_radacina)

--- userfs.sh
+++ userfs.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+cd "$HOME"
+if [[ ! -d proiect_itbi ]]; then
+    mkdir proiect_itbi
+    mkdir ./proiect_itbi/director_radacina 
+elif [[ ! -d ./proiect_itbi/director_radacina ]]; then
+    mkdir ./proiect_itbi/director_radacina
+fi
+
 cd "$HOME"/proiect_itbi/director_radacina 
 
 while true; do
-    a_users=($(users))
+    a_users=($(who | awk '{print $1}' | sort | uniq))
 
     for user in "${a_users[@]}"; do
 
-            #verificare daca exista directorul userului 
+        #verificare daca exista directorul userului 
         if [[ ! -d "$user" ]]; then
             mkdir ./"$user"
             touch ./"$user"/procs
         fi
         ps -u "$user" -o pid,comm>"$user"/procs
+        
         #stergerea fisierului 
         if [[ -f "./$user/lastlogin" ]]; then
             rm "./$user/lastlogin" 
-
         fi
-        
     done
 
     for user in *; do
+        
+        #verificare daca un user s-a deconectat
         inact=1
-
         for x in "${a_users[@]}"; do
             if [[ "$x" == "$user" ]]; then
                 inact=0
