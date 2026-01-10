6c9d5b13d3adb84d37780870b8b63987ad6befeb
09nicoleta <nicoleta.musat1@s.unibuc.ro>
2026-01-10 21:33:38 +0200

    Am modificat urmatoarele: 1) acum orice user adaugat care nu s-a logat va avea un director 2)cand un user este sters, directorul acestuia a fi eliminat 3)am adaugat 2>/dev/null dupa executarea comenzii ps, luand in considerare edge-case-urile  4)fisierul lastlogin nu se mai sterge, este ascuns cand userul este logat

--- userfs.sh
+++ userfs.sh
@@ -11,25 +11,37 @@ fi
 cd "$HOME"/proiect_itbi/director_radacina 
 
 while true; do
-    a_users=($(who | awk '{print $1}' | sort | uniq))
-
-    for user in "${a_users[@]}"; do
-
-        #verificare daca exista directorul userului 
-        if [[ ! -d "$user" ]]; then
+    
+    toti_userii=($( awk -F: '$3>999 && $1!="nobody" {print $1}' /etc/passwd ))
+        
+    #verificare daca exista directorul userului 
+    for user in "${toti_userii[@]}"; do
+        if [[ ! -d "$user" ]]; then 
             mkdir ./"$user"
             touch ./"$user"/procs
-        fi
-        ps -u "$user" -o pid,comm>"$user"/procs
+        fi   
+    done
+
+    #utilizatorii activi
+    a_users=($(who | awk '{print $1}' | sort | uniq))
+    for user in "${a_users[@]}"; do
+
+        ps -u "$user" -o pid,comm>"$user"/procs 2>/dev/null
         
-        #stergerea fisierului 
+        #ascunderea fisierului 
         if [[ -f "./$user/lastlogin" ]]; then
-            rm "./$user/lastlogin" 
+            mv "./$user/lastlogin" "./$user/.lastlogin" 
         fi
     done
 
     for user in *; do
         
+         #daca un user a fost sters
+        if ! grep -qw "$user" /etc/passwd; then
+            rm -r ./"$user"
+            continue
+        fi  
+
         #verificare daca un user s-a deconectat
         inact=1
         for x in "${a_users[@]}"; do
@@ -39,10 +51,11 @@ while true; do
             fi
         done
 
+        #user deconectat
         if [[ "$inact" == 1 ]]; then
             >./"$user"/procs
-            lastlog -u "$user" | tail -n 1 | awk '{print $4, $5, $6, $9, $7}'>./"$user"/lastlogin
-            
+            lastlog -u "$user" | tail -n 1 | awk '{print $4, $5, $6, $9, $7}'>./"$user"/.lastlogin
+            mv ./"$user"/.lastlogin ./"$user"/lastlogin
         fi
     done
     sleep 30
